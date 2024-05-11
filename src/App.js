9e40f088cc9b49b924ea049dcdd28461f145e0f8
FahadKHAN-4 <khanfahad0059@gmail.com>
2024-05-11 13:42:40 +0800

    routing issue

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import './App.css';
 import React, { useState } from 'react';
-import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import News from './components/News';
 import LoadingBar from 'react-top-loading-bar';
 
-
-function App () {
-  
-  const[progress, setProgress] = useState(30);
+function App() {
+  const [progress, setProgress] = useState(30);
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <News setProgress={setProgress} country="us" />,
+      element: <div>
+        <NavBar />
+        <LoadingBar
+          height={3}
+          color='#f11946'
+          progress={progress}
+        />
+        <News setProgress={setProgress} country="us" />
+      </div>,
     },
     {
       path: "/business",
@@ -38,22 +44,20 @@ function App () {
     {
       path: "/entertainment",
       element: <News setProgress={setProgress} category="entertainment" country="us" />,
-    } 
+    }
   ]);
 
-
-    return (
-      <div>
-        <NavBar />
-        <LoadingBar 
-          height= {3} 
-          color='#f11946'
-          progress={progress} />
-
-        <RouterProvider router={router}></RouterProvider>
-      </div>
-    );
-
+  return (
+    <div>
+      {/* <NavBar />
+            <LoadingBar
+                height={3}
+                color='#f11946'
+                progress={progress}
+            /> */}
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
